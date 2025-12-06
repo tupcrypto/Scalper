429ae9c51489840087a5a708a974b4f9d2d77b0a
tupcrypto <tup.crypto@gmail.com>
2025-12-06 22:34:33 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+BINGX_API_KEY       = "KvLedkuAig4Xun90oHe15AkYzDMBEjsGvJDNDpBaiWjTSuWnHHf0mAGVumQG9Tw2OS0Q23BjSh1TWzMxQ"
+BINGX_API_SECRET    = "Xk8aMKxFbwWht5QBIcEN5QBehiGlEQ4kU2jvt9nXk0S7zvTiGxoxe7VmcPkAnKUok2mpE9q7AWaiLMlUNyQ"
+
+TELEGRAM_BOT_TOKEN  = "8346338966:AAE9cuAqvDALtP77WkUjrgtnx1L3D6gME80"
+CHAT_ID             = "1048990805"     # for push notifications
+
+PAIR                = "BTC/USDT"
+LEVERAGE            = 2
+
+MAX_CAPITAL_PCT     = 25          # percentage of account used for trading
+GRID_ACTIVE         = False       # runtime state flag
