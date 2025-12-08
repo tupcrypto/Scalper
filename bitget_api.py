4c530722f4bc6fcce68678f905a325b4e8545de2
tupcrypto <tup.crypto@gmail.com>
2025-12-08 18:06:38 +0530

    Update bitget_api.py

--- bitget_api.py
+++ bitget_api.py
@@ -1,31 +1,27 @@
 import ccxt
 import config
 
+
 def get_exchange():
+    """
+    Bitget futures exchange (only used for placing orders when LIVE_TRADING=1
+    and for fetching prices).
+    """
     return ccxt.bitget({
-        "apiKey":    config.BITGET_API_KEY,
-        "secret":    config.BITGET_API_SECRET,
-        "password":  config.BITGET_API_PASSWORD,
+        "apiKey": config.BITGET_API_KEY,
+        "secret": config.BITGET_API_SECRET,
+        "password": config.BITGET_API_PASSWORD,
         "options": {
-            "defaultType": "swap",
-        }
+            "defaultType": "swap",   # USDT-M perpetual futures
+        },
     })
 
 
-# ---- NEW: simulated balance mode ----
-# we do NOT query Bitget, we assume user configured capital
-#
-def get_futures_balance():
-    try:
-        # use MAX_CAPITAL_PCT dynamically but scale 100%
-        # or let user set STATIC_BALANCE
-        return float(config.ASSUMED_BALANCE_USDT)
-    except:
-        return 0
-
-
-def get_price(symbol):
-    exchange = get_exchange()
-    ticker = exchange.fetch_ticker(symbol)
+def get_price(symbol: str) -> float:
+    """
+    Fetch last traded price for a futures symbol.
+    """
+    ex = get_exchange()
+    ticker = ex.fetch_ticker(symbol)
     return ticker["last"]
 
