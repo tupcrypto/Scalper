d1449533fd3e6df22f7f8b56827e48b00285ba45
tupcrypto <tup.crypto@gmail.com>
2025-12-12 11:10:03 +0530

    Update blofin_api.py

--- blofin_api.py
+++ blofin_api.py
@@ -1,18 +1,13 @@
-# blofin_api.py
 import ccxt.async_support as ccxt
-import config
 
 async def get_exchange():
-    """Initialize Blofin futures exchange"""
     exchange = ccxt.blofin({
-        "apiKey": config.BLOFIN_API_KEY,
-        "secret": config.BLOFIN_API_SECRET,
-        "password": config.BLOFIN_PASSWORD,
-        "enableRateLimit": True,
+        "apiKey": os.getenv("API_KEY"),
+        "secret": os.getenv("API_SECRET"),
+        "password": os.getenv("API_PASSWORD"),
         "options": {
             "defaultType": "swap"
         }
     })
-
     await exchange.load_markets()
     return exchange
